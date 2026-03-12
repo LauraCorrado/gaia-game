b64e36f03e22b694b81ab24006b194f80e8949cd
Laura Corrado <laura@teacz.com>
2026-03-12 11:56:14 +0100

    feat(main.tsx): inizializzato dark mode prima del render

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { initDarkMode } from './utils/darkMode'
+
+initDarkMode()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
