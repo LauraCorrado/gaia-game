3c779eeeba78dbd883dcd47b93e8dd53acf5b382
Laura Corrado <laura@teacz.com>
2026-03-12 15:50:19 +0100

    feat(App.tsx): impostate rotte con react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/shared/Layout'
+import Home from './pages/Home'
+import ScopriGaia from './pages/ScopriGaia'
+import Team from './pages/Team'
+import EventiNews from './pages/EventiNews'
+import DietroLeQuinte from './pages/DietroLeQuinte'
+import Contatti from './pages/Contatti'
+import PrivacyPolicy from './pages/PrivacyPolicy'
+import CookiePolicy from './pages/CookiePolicy'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        {/* <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/scopri-gaia" element={<ScopriGaia />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/eventi-news" element={<EventiNews />} />
+          <Route path="/dietro-lequinte" element={<DietroLeQuinte />} />
+          <Route path="/contatti" element={<Contatti />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/cookie-policy" element={<CookiePolicy />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App
