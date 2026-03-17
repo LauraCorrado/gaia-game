d75d5537ae194bdc60d4c89f5503025afce8e966
Laura Corrado <laura@teacz.com>
2026-03-17 11:41:06 +0100

    feat(Layout.tsx): aggiunto InstitutionalBanner component e formattato il codice

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -2,20 +2,28 @@ import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
 import CookieBanner from './CookieBanner'
+import InstitutionalBanner from './InstitutionalBanner'
 
 export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
-      
-       <a href="#main-content"
-        className="sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4 focus:z-50 focus:px-4 focus:py-2 focus:rounded focus:bg-lm-blue focus:text-white dark:focus:bg-dm-blue dark:focus:text-lm-text-primary"
+
+      <a
+        href="#main-content"
+        className="sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4 focus:z-60 focus:px-4 focus:py-2 focus:rounded focus:bg-lm-blue focus:text-white dark:focus:bg-dm-blue dark:focus:text-lm-text-primary"
       >
         Vai al contenuto principale
       </a>
+
+      <InstitutionalBanner />
       <Header />
-      <main id="main-content" className="grow">
-          <Outlet />
+
+      <main
+        id="main-content" className='grow'
+      >
+        <Outlet />
       </main>
+
       <Footer />
       <CookieBanner />
     </div>
