064e2f0a37f83fa9f339866c8ce3939ffef74c3c
Laura Corrado <laura@teacz.com>
2026-03-18 12:35:02 +0100

    feat(Footer.tsx): updated footer layout with new logos and improved content structure

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -7,28 +7,12 @@ export default function Footer() {
   return (
     <footer className="mt-auto bg-lm-bg-secondary dark:bg-dm-bg-secondary border-t border-lm-bg-secondary dark:border-dm-bg-secondary">
       <div className="h-0.5 w-full bg-linear-to-r from-lm-pink via-lm-blue to-lm-green" />
-
-      <div className="max-w-6xl mx-auto px-4 py-10 flex flex-col md:flex-row items-center justify-between gap-6 text-sm">
-        <p>
-          © {year} <span className="font-semibold">GAIA</span> – Progetto
-          finanziato dalla Regione Calabria
-        </p>
-
-        <nav className="flex gap-6">
-          <NavLink to="/privacy-policy">Privacy Policy</NavLink>
-          <NavLink to="/cookie-policy">Cookie Policy</NavLink>
-        </nav>
-
-        <p className="text-xs opacity-70">TEA srl | {year}</p>
-      </div>
-
       <div className="max-w-6xl mx-auto px-4 border-t border-lm-bg-secondary dark:border-dm-bg-secondary py-8">
         <h4 className="text-center font-semibold mb-4">Menu</h4>
 
         <ul className="flex flex-wrap justify-center gap-3">
           {navLinks.map(({ to, label }) => (
             <li key={to}>
-              
               <NavLink
                 to={to}
                 className={({ isActive }) =>
@@ -46,15 +30,56 @@ export default function Footer() {
               >
                 <span className="relative z-10">{label}</span>
 
-                {/* accent underline */}
                 <span className="absolute inset-0 pointer-events-none">
-                  <span className="absolute top-0 left-0 w-2 h-2 border-t border-l border-current opacity-40" />
-                  <span className="absolute bottom-0 right-0 w-2 h-2 border-b border-r border-current opacity-40" />
+                  <span className="absolute top-0 left-0 w-2 h-2 border-t border-l border-current rounded-xs opacity-40" />
+                  <span className="absolute bottom-0 right-0 w-2 h-2 border-b border-r border-current rounded-xs opacity-40" />
                 </span>
               </NavLink>
             </li>
           ))}
         </ul>
+      </div>
+      <div className="max-w-6xl mx-auto px-4 py-10 flex flex-col md:flex-row items-center justify-between gap-6 text-sm">
+        <div className="flex flex-col items-center justify-center gap-6 text-center md:text-left">
+          <img
+            src="/logos/gaia_logo.png"
+            alt="GAIA – Giochi Accessibili ed Inclusivi per bambini con Autismo"
+            className="h-20 w-auto"
+          />
+
+          <p className="text-sm">
+            © {year} –{" "}
+            <strong>
+              Giochi Accessibili ed Inclusivi per bambini con Autismo
+            </strong>
+          </p>
+        </div>
+
+        <nav className="flex gap-6">
+          <NavLink to="/privacy-policy">Privacy Policy</NavLink>
+          <NavLink to="/cookie-policy">Cookie Policy</NavLink>
+        </nav>
+
+        <a
+          href="https://www.teacz.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm opacity-70 gap-2 flex items-center justify-center flex-col hover:opacity-100 transition"
+        >
+          <img
+            src="/logos/tea_logo.png"
+            alt="Logo TEA srl"
+            className="h-10 w-auto block dark:hidden"
+          />
+
+          <img
+            src="/logos/tea_logo_neg.png"
+            alt="Logo TEA srl"
+            className="h-10 w-auto hidden dark:block"
+          />
+
+          <span>Sito realizzato da TEA srl | {year}</span>
+        </a>
       </div>
     </footer>
   );
